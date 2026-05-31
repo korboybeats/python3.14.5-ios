58bdddea7eb183019167c44faf5d7e01206fffb6
korboybeats <korboybeats@gmail.com>
2026-05-31 23:44:02 +0900

    Use Homebrew Python on Mac mini CI

--- scripts/install-build-tools.sh
+++ scripts/install-build-tools.sh
@@ -9,7 +9,7 @@ set -euxo pipefail
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
-FORMULAE=(dpkg ldid autoconf automake libtool pkg-config coreutils gnu-sed cmake nasm yasm git wget gpatch)
+FORMULAE=(dpkg ldid autoconf automake libtool pkg-config coreutils gnu-sed cmake nasm yasm git wget gpatch python@3.14)
 
 for f in "${FORMULAE[@]}"; do
   if brew list --formula | grep -qx "${f}"; then
